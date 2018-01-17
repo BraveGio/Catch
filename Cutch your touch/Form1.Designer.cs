d96ba1e0f537b34c9d0c1b49681a022840549d47
Adam <adamkita@tlen.pl>
2018-01-17 21:57:31 +0100

    początek

--- Cutch your touch/Form1.Designer.cs
+++ Cutch your touch/Form1.Designer.cs
@@ -29,11 +29,90 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.przyciskScreen = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.podglad = new System.Windows.Forms.PictureBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.podglad)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.przyciskScreen);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(180, 590);
+            this.panel1.TabIndex = 0;
+            // 
+            // przyciskScreen
+            // 
+            this.przyciskScreen.Dock = System.Windows.Forms.DockStyle.Top;
+            this.przyciskScreen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.przyciskScreen.Location = new System.Drawing.Point(0, 0);
+            this.przyciskScreen.Name = "przyciskScreen";
+            this.przyciskScreen.Size = new System.Drawing.Size(178, 32);
+            this.przyciskScreen.TabIndex = 0;
+            this.przyciskScreen.Text = "Screen";
+            this.przyciskScreen.UseVisualStyleBackColor = true;
+            this.przyciskScreen.MouseDown += new System.Windows.Forms.MouseEventHandler(this.przyciskScreen_MouseDown);
+            this.przyciskScreen.MouseUp += new System.Windows.Forms.MouseEventHandler(this.przyciskScreen_MouseUp);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(180, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(797, 32);
+            this.panel2.TabIndex = 1;
+            // 
+            // podglad
+            // 
+            this.podglad.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.podglad.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.podglad.Location = new System.Drawing.Point(180, 32);
+            this.podglad.Name = "podglad";
+            this.podglad.Size = new System.Drawing.Size(797, 558);
+            this.podglad.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.podglad.TabIndex = 2;
+            this.podglad.TabStop = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "542729.png");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(977, 590);
+            this.Controls.Add(this.podglad);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.podglad)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button przyciskScreen;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox podglad;
+        private System.Windows.Forms.ImageList imageList1;
     }
 }
 
--- Cutch your touch/Form1.cs
+++ Cutch your touch/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,26 @@ namespace Cutch_your_touch
         public Form1()
         {
             InitializeComponent();
+        }
+        
+      
+
+        private void przyciskScreen_MouseDown(object sender, MouseEventArgs e)
+        {
+            //Form.ActiveForm.Hide();
+            Bitmap bitmap = new Bitmap(Screen.PrimaryScreen.Bounds.Width,
+                Screen.PrimaryScreen.Bounds.Height);
+            Bitmap bitmap2=new Bitmap(Screen.AllScreens[0].Bounds.Width,Screen.AllScreens[0].Bounds.Height,PixelFormat.Format32bppArgb);
+            Graphics graphics = Graphics.FromImage(bitmap2 as Image);
+            graphics.CopyFromScreen(0, 0, 0, 0, bitmap2.Size);
+            podglad.Image = bitmap2;
+            bitmap2.Save(@"C:\screen\screen" + Guid.NewGuid() + ".jpg", ImageFormat.Jpeg);
+            
+        }
+
+        private void przyciskScreen_MouseUp(object sender, MouseEventArgs e)
+        {
+            
         }
     }
 }
